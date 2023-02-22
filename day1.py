c858d2430ce1226c8057f9ea88c5447f0bddbe2a
kasthu271 <126040003+kasthu271@users.noreply.github.com>
2023-02-23 01:26:49 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,20 @@
+frist_name="Kasthuri"
+age=20
+sum=(frist_name,age)
+print(sum)
+name=input("what is your name?" )
+print("hello my dear" + name)
+first=input("first:" )
+second=input("second:" )
+sum=int(first) + int(second)
+print(sum)
+first=input("first:" )
+second=input("second:" )
+sum=float(first) + float(second)
+print("sum:" + str(sum))
+working="student in srm"
+print(working.upper())
+print(working.lower())
+print(working.find('in'))
+print(working.replace('in', '22'))
+
