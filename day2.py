91e0c80a3d1763411012020cf7da6c2d79c0923a
kasthu271 <126040003+kasthu271@users.noreply.github.com>
2023-02-23 22:54:15 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,55 @@
+last_name="Kasthuri"
+first_name="Padarthi"
+gender="female"
+subject_marks=("maths,physcics")
+maths=input("maths:" )
+physics=input("physics:" )
+sum=(last_name,first_name,gender,maths,subject_marks,physics)
+print(sum)
+
+
+
+OPERATOR PRECEDENCE
+x= (10+30*4)
+print(x)
+
+x=(20+90)*40
+print(x)
+
+x=3>2
+print(x)
+
+x=3==2
+print(x)
+
+x=3!=2
+print(x)
+
+
+LOGICAL OPERATOR
+amount=400
+print(amount>300 and amount<400)
+price=30
+print(price>40 or price<31)
+print(not price>40)
+print(price>32)
+
+
+three logical operators 
+and(both)
+or(at least one)
+not
+
+IF STATEMENT
+temperature=90
+if temperature>82:
+    print("its a hot day")
+    print("drink plenty of water")
+    
+temperature=69
+if temperature<70:
+    print("its not a hot day")
+    
+
+
+
