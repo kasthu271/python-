3d50e840ba5192fb40ff2f3519cda8f1868a4281
kasthu271 <126040003+kasthu271@users.noreply.github.com>
2023-02-25 00:39:12 +0530

    Create simplecalculator.py

---
+++ simplecalculator.py
@@ -0,0 +1,14 @@
+x=int(input("enter the first number: "))
+y=int(input("enter the second number: "))
+op=input("enter the operator =,-,*,/: ")
+if op=="+":
+    print(x+y)
+elif op=="-":
+    print(x-y)
+elif op=="*":
+    print(x*y)
+elif op=="/":
+    print(x/y)
+else:
+    print("invalid")
+ 
