4f38e6062e420a07bd2a61d4c15312d391c688ea
kasthu271 <126040003+kasthu271@users.noreply.github.com>
2023-02-24 19:51:01 +0530

    Create day3half.py

---
+++ day3half.py
@@ -0,0 +1,44 @@
+IN POUNDS
+weight=int(input("weight:"))
+unit=input("(K)g,(L)bs:" )
+if unit.upper()=="k":
+    converted=weight/0.45
+    print("weight in lbs:" + converted)
+else:
+    converted=weight*0.45
+    print("weight in kg:" + str(converted))
+      
+	  IN KG 
+weight= int(input("weight:" ))
+unit=input("(K)g,(L)bs:" )
+if unit.upper()=="k":
+    converted=weight/0.45
+    print("weight in lbs:" +str converted)
+else:
+    converted=weight*0.45
+    print("weight in kg:" + str(converted))
+      	  
+	WHILE LOOPS
+	
+	i=1
+while i<=547:
+    print(i)
+    i=i+1
+	
+	k=8
+while k<=1_00: ( "_" CHOICE)
+    print(k)
+    k=k+8
+	
+	k=1
+while k<=10:
+    print(k * '52')
+    k=k+1
+	
+	LISTS ( "[]" )
+names=["chinnu","honey","smiley","ammu"]
+print(names)
+names[0]="chinu"
+print(names[0])
+print(names)
+print(names[0:4])
