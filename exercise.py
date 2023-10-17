0cd53766302f2b01b5c9e61cea67f8d99f4337b8
kasthu271 <126040003+kasthu271@users.noreply.github.com>
2023-10-17 23:52:14 +0530

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age: "))
+years_left=90-age
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*52
+print(f"you have {days_left} days,{weeks_left} weeks,{months_left} left")
+
+#calculate the days ,weeks,months that ur have and u need too be till  90 yrs
+#exercise
